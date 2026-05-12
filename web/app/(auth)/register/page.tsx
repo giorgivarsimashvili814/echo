45f2d691cbd1654b5243e5ac559abb8746dbed6f
unknown <giorgivarsimashvili814@gmail.com>
2026-05-13 01:55:06 +0400

    fix: change server url

--- web/app/(auth)/register/page.tsx
+++ web/app/(auth)/register/page.tsx
@@ -34,7 +34,7 @@ export default function RegisterPage() {
 
   const onSubmit = async (data: RegisterForm) => {
     try {
-      const res = await fetch("http://localhost:3001/auth/register", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
